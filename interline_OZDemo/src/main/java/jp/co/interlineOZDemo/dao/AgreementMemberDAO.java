6a4f0c4e93b62463f5d970d6447a836e3c116075
LAPTOP-8M79HCKV\Administrator <navyya20@gmail.com>
2020-10-16 17:46:40 +0900

    동의서 시스템을 위한 컨트롤러 메퍼 다오 등등 준비  (VO는 각자)

---
+++ interline_OZDemo/src/main/java/jp/co/interlineOZDemo/dao/AgreementMemberDAO.java
@@ -0,0 +1,22 @@
+package jp.co.interlineOZDemo.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import jp.co.interlineOZDemo.vo.BillInformVO;
+import jp.co.interlineOZDemo.vo.EstimateItemsVO;
+import jp.co.interlineOZDemo.vo.EstimateSheetVO;
+import jp.co.interlineOZDemo.vo.UserInformVO;
+
+@Repository
+public class AgreementMemberDAO {
+
+	@Autowired
+	SqlSession session;
+	
+}
---
+++ interline_OZDemo/src/main/java/jp/co/interlineOZDemo/dao/AgreementMemberMapper.java
@@ -0,0 +1,15 @@
+package jp.co.interlineOZDemo.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.session.RowBounds;
+
+import jp.co.interlineOZDemo.vo.BillInformVO;
+import jp.co.interlineOZDemo.vo.EstimateItemsVO;
+import jp.co.interlineOZDemo.vo.EstimateSheetVO;
+import jp.co.interlineOZDemo.vo.UserInformVO;
+
+public interface AgreementMemberMapper {
+
+}
